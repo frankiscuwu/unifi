f53898d5e42b9a59829c71a458b96d39bcbc47b5
Joseph Marotta <marottaj@bu.edu>
2025-10-11 14:12:10 -0400

    Add to auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,17 +1,31 @@
-import NextAuth from "next-auth";
-import SpotifyProvider from "next-auth/providers/spotify";
+import NextAuth, { AuthOptions } from 'next-auth';
+import SpotifyProvider from 'next-auth/providers/spotify';
 
-export const authOptions = {
-    providers: [
-        SpotifyProvider({
-            clientId: process.env.SPOTIFY_CLIENT_ID!,
-            clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
-        }),
-    ],
-    secret: process.env.NEXTAUTH_SECRET
+export const authOptions: AuthOptions = {
+  providers: [
+    SpotifyProvider({
+      clientId: process.env.SPOTIFY_CLIENT_ID!,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
+      authorization:
+        'https://accounts.spotify.com/authorize?scope=user-read-email,user-modify-playback-state,user-read-playback-state',
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+        token.refreshToken = account.refresh_token;
+        token.expires = account.expires_at! * 1000; 
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken;
+      session.refreshToken = token.refreshToken;
+      return session;
+    },
+  },
 };
 
 const handler = NextAuth(authOptions);
-
-// App Router API routes expect named exports for HTTP methods.
 export { handler as GET, handler as POST };
