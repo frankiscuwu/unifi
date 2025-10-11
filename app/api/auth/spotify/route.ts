ef672f1219b310a1457b1d884d30d8b9887d9706
Frank Yang <frankyang009@gmail.com>
2025-10-11 12:57:59 -0400

    da

--- app/api/auth/spotify/route.ts
+++ app/api/auth/spotify/route.ts
@@ -1,6 +1,6 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
-export async function GET(request: NextRequest) {
+export async function GET() {
   const clientId = process.env.SPOTIFY_CLIENT_ID;
   
   if (!clientId) {
